f7021097e5a796eb5ff36826fe87c9398bfc3829
Essonti <Strofim4enko@yandex.ru>
2021-08-28 10:39:47 +0300

    feat: add code and pass all tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  //function check(str, bracketsConfig) {
+  let 
+  br = '', 
+  strLengthReduce = 0;
+  
+  for (j = 0; j < 2 * bracketsConfig.length; j++) {
+    
+    for (i = 0; i < bracketsConfig.length; i++) {
+      br = bracketsConfig[i][0] + bracketsConfig[i][1];      
+      strLengthReduce = str.length/2;
+      
+      for (k = 0; k < strLengthReduce; k++) {
+        str = str.replace(br, '');
+      }
+
+    }
+
+  }
+  //console.log(str);
+  if (str == '') {return true} else {return false;}
+  
 }
